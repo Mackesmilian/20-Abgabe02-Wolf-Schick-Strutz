fc8f834e7ed74c7dbc087b015b0fb3ae7f4ff0a3
Dominik Schick <dominikschick@outlook.com>
2020-07-01 10:45:24 +0200

    Added JavaDoc comments

--- src/main/java/at/fh/bsd/GenericQueue.java
+++ src/main/java/at/fh/bsd/GenericQueue.java
@@ -9,10 +9,22 @@ public class GenericQueue implements IQueue<Object> {
     private List<Object> elements = new ArrayList<>();
     private int maxSize;
 
+    /**
+     * Constructor for GenericQueue.
+     * Makes a new GenericQueue object with initialisation of an integer.
+     *
+     * @param maxSize determines the maximum size for the queue
+     */
     public GenericQueue(int maxSize) {
         this.maxSize = maxSize;
     }
 
+    /**
+     * If you call this function it returns a boolean if the size of the list is not equal the maxSize.
+     *
+     * @param obj the object given the method
+     * @return returns a boolean; true if the object was successfully added, false if not
+     */
     @Override
     public boolean offer(Object obj) {
         if (elements.size() != maxSize) {
@@ -23,6 +35,12 @@ public class GenericQueue implements IQueue<Object> {
         return true;
     }
 
+    /**
+     * Gets the first object in the list then deletes it and returns the value, if not null.
+     * If the list is of size 0 this method returns 0 and deletes nothing.
+     *
+     * @return the object that stands first in the list which got deleted or null
+     */
     @Override
     public Object poll() {
         Object element = peek();
@@ -33,6 +51,12 @@ public class GenericQueue implements IQueue<Object> {
         return element;
     }
 
+    /**
+     * Removes the first object by calling poll() if the list is not of size 0.
+     * If the list size is 0 there will be thrown an exception.
+     *
+     * @return calls the poll() method to delete and returns the object that got deleted
+     */
     @Override
     public Object remove() {
         if(elements.size() == 0){
@@ -41,6 +65,11 @@ public class GenericQueue implements IQueue<Object> {
         return poll();
     }
 
+    /**
+     * If the list is bigger then 0 the method returns the first object, otherwise returns null.
+     *
+     * @return the first element in the list or null
+     */
     @Override
     public Object peek() {
         Object element;
@@ -52,6 +81,12 @@ public class GenericQueue implements IQueue<Object> {
         return element;
     }
 
+    /**
+     * Gets the first element in the list if the element is not null.
+     * If the element is null there will be thrown an exception.
+     *
+     * @return the first element in the list
+     */
     @Override
     public Object element() {
         Object element = peek();
